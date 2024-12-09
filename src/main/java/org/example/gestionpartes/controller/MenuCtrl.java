a76432441aeac6f82efc81b4562dcca3d07cd7d5
saulm <saul.mancas@educa.jcyl.es>
2024-12-09 12:21:35 +0100

    arreglado fallo emn variable y cambio el save por persist

--- src/main/java/org/example/gestionpartes/controller/MenuCtrl.java
+++ src/main/java/org/example/gestionpartes/controller/MenuCtrl.java
@@ -3,11 +3,9 @@ package org.example.gestionpartes.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import org.example.gestionpartes.model.Profesor;
 import org.example.gestionpartes.model.TipoProfesor;
 import org.example.gestionpartes.service.GestionPartesService;
@@ -52,31 +50,26 @@ public class MenuCtrl implements Initializable {
     @FXML
     void clickCrearParte(ActionEvent event) {
         SceneManager.changeScene(event, "crearparte-view.fxml");
-
     }
 
     @FXML
     void clickCrearProfesor(ActionEvent event) {
         SceneManager.changeScene(event, "crear_profesor-view.fxml");
-
     }
 
     @FXML
     void clickListaAlumnos(ActionEvent event) {
         SceneManager.changeScene(event, "lista_alumnos-view.fxml");
-
     }
 
     @FXML
     void clickListaPartes(ActionEvent event) {
         SceneManager.changeScene(event, "lista_partes-view.fxml");
-
     }
 
     @FXML
     void onCloseSessionClick(ActionEvent event) {
         AlertShow.confirmation("¿Desea cerrar la sesión?");
         SceneManager.changeScene(event, "login-view.fxml");
-
     }
 }
