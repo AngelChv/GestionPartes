5fda3bf6534b6e492903457af4536de81afde6f3
Mario <mario.ganfer@educa.jcyl.es>
2024-12-09 13:21:56 +0100

    Crear parte faltan las opciones del rojo

--- src/main/java/org/example/gestionpartes/controller/CrearPartesCtrl.java
+++ src/main/java/org/example/gestionpartes/controller/CrearPartesCtrl.java
@@ -5,8 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
-import org.example.gestionpartes.DAO.AlumnoDAO;
-import org.example.gestionpartes.DAO.AlumnoDAOImpl;
+import javafx.scene.paint.Color;
+import org.example.gestionpartes.DAO.*;
 import org.example.gestionpartes.model.Alumno;
 import org.example.gestionpartes.model.ColorParte;
 import org.example.gestionpartes.model.Parte;
@@ -18,8 +18,7 @@ import org.example.gestionpartes.util.Validator;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ResourceBundle;
-import java.util.Set;
+import java.util.*;
 
 public class CrearPartesCtrl implements Initializable {
     @FXML
@@ -50,11 +49,19 @@ public class CrearPartesCtrl implements Initializable {
 
     private final static AlumnoDAOImpl alumnoDAO = new AlumnoDAOImpl();
 
-    //private final static Set<TipoParte> tiposParte = new ;
+    private final static ParteDAOImpl parteDAO = new ParteDAOImpl();
+
+    private static final Map<ColorParte, TipoParte> tiposParteMap = new HashMap<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         onVerdeClick();
+
+        TipoParteDAOImpl tipoParteDAO = new TipoParteDAOImpl();
+        for (TipoParte t : tipoParteDAO.findAll()) {
+            tiposParteMap.put(t.getColor(), t);
+        }
+
         // Llenar ComboBox con valores
         for (int i = 8; i <= 22; i++) {
             horaComboBox.getItems().add(String.format("%02d", i));
@@ -66,51 +73,69 @@ public class CrearPartesCtrl implements Initializable {
         // Configuración inicial
         horaComboBox.setValue("00");
         minutoComboBox.setValue("00");
+
+
     }
 
     @FXML
     void onNaranjaClick(ActionEvent event) {
         vBox.setStyle("-fx-background-color: #faac46;");
         titleLabel.setText("Parte naranja de nota negativa");
-        color=ColorParte.NARANJA;
+        color = ColorParte.NARANJA;
     }
 
     @FXML
     void onRojoClick(ActionEvent event) {
         vBox.setStyle("-fx-background-color: #d06f6f;");
         titleLabel.setText("Parte rojo de nota negativa");
-        color=ColorParte.ROJO;
+        color = ColorParte.ROJO;
     }
 
     @FXML
     void onVerdeClick() {
         vBox.setStyle("-fx-background-color: #9af69f;");
         titleLabel.setText("Parte verde de advertencia");
-        color=ColorParte.VERDE;
+        color = ColorParte.VERDE;
     }
 
     @FXML
     void onSaveClick(ActionEvent event) {
         //Obtener los valores de los campos
-        String numExpediente=numexpedienteTxt.getText();
-        LocalDate fecha=datePick.getValue();
-        String descripcion=descripcionTxt.getText();
-        String sancion=sancionTxt.getText();
+        String numExpediente = numexpedienteTxt.getText();
+        LocalDate fecha = datePick.getValue();
+        String descripcion = descripcionTxt.getText();
+        String sancion = sancionTxt.getText();
         String hora = horaComboBox.getSelectionModel().getSelectedItem();
         String minuto = minutoComboBox.getSelectionModel().getSelectedItem();
 
-        if (numExpediente.isEmpty() || fecha==null || hora==null || minuto==null
-            || descripcion.isEmpty() || sancion.isEmpty()){
+        if (numExpediente.isEmpty() || fecha == null || hora == null || minuto == null
+                || descripcion.isEmpty() || sancion.isEmpty()) {
             AlertShow.warning("Por favor. Rellene todos los campos.");
-        } else if (!Validator.validarNumExp(numExpediente)){
+        } else if (!Validator.validarNumExp(numExpediente)) {
             AlertShow.warning("Introduzca un numero de expediente correcto.");
         } else { //Todos los campos completados y el numero de expediente es valido.
             Alumno alumno = alumnoDAO.findByNumExp(Integer.parseInt(numExpediente));
-            if (alumno==null){
-                AlertShow.warning("No existe un alumno con número de expediente: "+numExpediente);
+            if (alumno == null) {
+                AlertShow.warning("No existe un alumno con número de expediente: " + numExpediente);
             } else {
                 LocalTime time = LocalTime.of(Integer.parseInt(hora), Integer.parseInt(minuto));
-              //  Parte parte = new Parte(alumno, GestionPartesService.getProfesor(),descripcion,fecha,time,sancion, color);
+
+                Parte parte = new Parte(
+                        alumno,
+                        GestionPartesService.getProfesor(),
+                        descripcion,
+                        fecha,
+                        time,
+                        sancion,
+                        tiposParteMap.get(color)
+                );
+
+                if(parteDAO.crear(parte)){
+                    AlertShow.info("Parte creado correctamente.");
+                } else {
+                    AlertShow.error("No se pudo crear el parte.");
+                }
+
             }
         }
     }
