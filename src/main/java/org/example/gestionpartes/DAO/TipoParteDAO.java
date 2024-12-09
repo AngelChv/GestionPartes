8705839277bc493012853e2a77a7d0c6323540af
Angel Chicote <angel.chiveg@educa.jcyl.es>
2024-12-09 12:21:00 +0100

    Dao tipo parte

---
+++ src/main/java/org/example/gestionpartes/DAO/TipoParteDAO.java
@@ -0,0 +1,9 @@
+package org.example.gestionpartes.DAO;
+
+import org.example.gestionpartes.model.TipoParte;
+
+import java.util.List;
+
+public interface TipoParteDAO {
+    public List<TipoParte> findAll();
+}
