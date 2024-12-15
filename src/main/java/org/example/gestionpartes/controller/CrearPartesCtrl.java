daf9307306d5be4db26d0bec6eef67a34a38774a
Mario <mario.ganfer@educa.jcyl.es>
2024-12-15 19:19:37 +0100

    Corregido parte rojo

--- src/main/java/org/example/gestionpartes/controller/CrearPartesCtrl.java
+++ src/main/java/org/example/gestionpartes/controller/CrearPartesCtrl.java
@@ -1,11 +1,12 @@
 package org.example.gestionpartes.controller;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import org.example.gestionpartes.DAO.*;
 import org.example.gestionpartes.model.Alumno;
 import org.example.gestionpartes.model.ColorParte;
@@ -43,13 +44,15 @@ public class CrearPartesCtrl implements Initializable {
     private TextArea descripcionTxt;
 
     @FXML
+    private Label sancionLabel;
+
+    @FXML
     private TextArea sancionTxt;
 
     @FXML
-    private ComboBox<String> opcionesParteRojo;
+    private Button guardarBttn;
 
-    @FXML
-    private Label sancionRojoLabel;
+    private ComboBox<String> tipoSancionCBox;
 
     private ColorParte color;
 
@@ -77,45 +80,85 @@ public class CrearPartesCtrl implements Initializable {
         }
 
         // Configuración inicial
-        horaComboBox.setValue("00");
+        horaComboBox.setValue("08");
         minutoComboBox.setValue("00");
 
+        // Asignar tipos sancion comboBox.
+        ArrayList<String> tiposSancion = new ArrayList<>();
+        tiposSancion.add("Incoación de expediente o en su caso expediente abreviado.");
+        tiposSancion.add("Reunión con la Comisión de Convivencia.");
+        tiposSancion.add("Es obligatorio pedir disculpas a la persona/as contra las que se ejerció daño físico o moral, y/o reparar los daños materiales causados");
+        tiposSancion.add("Otra");
+        tipoSancionCBox = new ComboBox<>(FXCollections.observableArrayList(tiposSancion));
+        tipoSancionCBox.setPromptText("Sanción que recibes por jefatura de Estudios:");
 
+        // Evento del comboBox:
+        tipoSancionCBox.getSelectionModel().selectedItemProperty().addListener((_, _, newValue) -> {
+            if ("Otra".equals(newValue)) {
+                // Añadir la sanción al VBox
+                if (!vBox.getChildren().contains(sancionTxt)) {
+                    vBox.getChildren().add(vBox.getChildren().indexOf(guardarBttn), sancionTxt);
+                    VBox.setVgrow(sancionTxt, Priority.ALWAYS);
+                }
+            } else {
+                // Eliminar la sanción si está presente
+                vBox.getChildren().remove(sancionTxt);
+            }
+        });
     }
 
     @FXML
     void onRojoClick(ActionEvent event) {
+        // Cambiar color de fondo
         vBox.setStyle("-fx-background-color: #d06f6f;");
         titleLabel.setText("Parte rojo de nota negativa");
         color = ColorParte.ROJO;
 
-        //Cambiar TextArea por comboBox
-//        sancionTxt.setVisible(false);
-//        sancionRojoLabel.setVisible(true);
-//        opcionesParteRojo.setVisible(true);
+        vBox.getChildren().remove(sancionLabel);
+        vBox.getChildren().remove(sancionTxt);
+
+        // Añadir tipoSancionCBox por encima del botón "Guardar"
+        if (!vBox.getChildren().contains(tipoSancionCBox)) {
+            vBox.getChildren().add(vBox.getChildren().indexOf(guardarBttn), tipoSancionCBox);
+        }
     }
 
     @FXML
     void onNaranjaClick(ActionEvent event) {
+        // Cambiar color de fondo
         vBox.setStyle("-fx-background-color: #faac46;");
         titleLabel.setText("Parte naranja de nota negativa");
         color = ColorParte.NARANJA;
 
-        //Volver a mostrar el TextArea
-//        sancionTxt.setVisible(true);
-//        opcionesParteRojo.setVisible(false);
+        configureTextArea();
     }
 
     @FXML
     void onVerdeClick() {
+        // Cambiar color de fondo
         vBox.setStyle("-fx-background-color: #9af69f;");
         titleLabel.setText("Parte verde de advertencia");
         color = ColorParte.VERDE;
 
-        //Volver a mostrar el TextArea
-//        sancionTxt.setVisible(true);
-//        opcionesParteRojo.setVisible(false);
+        configureTextArea();
     }
+
+    private void configureTextArea() {
+        // Eliminar el ComboBox de tipos de sanción si está presente
+        vBox.getChildren().remove(tipoSancionCBox);
+
+        // Añadir sancionLabel por encima del botón "Guardar"
+        if (!vBox.getChildren().contains(sancionLabel)) {
+            vBox.getChildren().add(vBox.getChildren().indexOf(guardarBttn), sancionLabel);
+            // Reasignar vgrow explícitamente
+        }
+
+        if (!vBox.getChildren().contains(sancionTxt)) {
+            vBox.getChildren().add(vBox.getChildren().indexOf(guardarBttn), sancionTxt);
+            VBox.setVgrow(sancionTxt, Priority.ALWAYS);
+        }
+    }
+
 
     @FXML
     void onSaveClick(ActionEvent event) {
@@ -126,23 +169,23 @@ public class CrearPartesCtrl implements Initializable {
         String sancion = sancionTxt.getText();
         String hora = horaComboBox.getSelectionModel().getSelectedItem();
         String minuto = minutoComboBox.getSelectionModel().getSelectedItem();
+        String tipoSancion = tipoSancionCBox.getSelectionModel().getSelectedItem();
 
-        //Saber si hay que coger la info del comboBox o del textArea
-//        if (opcionesParteRojo.isVisible()){
-//            sancion=opcionesParteRojo.getSelectionModel().getSelectedItem();
-//            if (sancion==null){
-//                AlertShow.warning("Seleccione una opcion.");
-//            }
-//        } else {
-//            sancion=sancionTxt.getText();
-//        }
 
         if (numExpediente.isEmpty() || fecha == null || hora == null || minuto == null
-                || descripcion.isEmpty() || sancion.isEmpty()) {
+                || descripcion.isEmpty()) { // Si los campos comunes no están rellenos.
             AlertShow.warning("Por favor. Rellene todos los campos.");
+        } else if (color != ColorParte.ROJO && sancion.isEmpty()) { // Si los campos de parte verde y naranja no están rellenos.
+            AlertShow.warning("Por favor, introduzca una sanción.");
+        } else if (color == ColorParte.ROJO && tipoSancion == null) {
+            // Si no se ha elegido una sanción en el parte rojo
+            AlertShow.warning("Por favor, seleccione una sanción.");
+        } else if (color == ColorParte.ROJO && tipoSancion.equals("Otra") && sancion.isEmpty()) {
+            // Si no se ha rellenado la sanción personalizada.
+            AlertShow.warning("Por favor, rellene la sanción personalizada.");
         } else if (!Validator.validarNumExp(numExpediente)) {
             AlertShow.warning("Introduzca un numero de expediente correcto.");
-        } else { //Todos los campos completados y el numero de expediente es valido.
+        } else { //Todos los campos completados y el número de expediente es válido.
             Alumno alumno = alumnoDAO.findByNumExp(Integer.parseInt(numExpediente));
             if (alumno == null) {
                 AlertShow.warning("No existe un alumno con número de expediente: " + numExpediente);
@@ -159,7 +202,7 @@ public class CrearPartesCtrl implements Initializable {
                         tiposParteMap.get(color)
                 );
 
-                if(parteDAO.crear(parte)){
+                if (parteDAO.crear(parte)) {
                     AlertShow.info("Parte creado correctamente.");
                 } else {
                     AlertShow.error("No se pudo crear el parte.");
@@ -168,5 +211,4 @@ public class CrearPartesCtrl implements Initializable {
             }
         }
     }
-
 }
